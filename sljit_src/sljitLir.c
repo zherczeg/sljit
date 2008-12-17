87cf91ee660421367155e2eae3d09d351b3a294f
Zoltan Herczeg <zherczeg.u-szeged@partner.samsung.com>
2008-12-17 09:06:19 +0000

    Initial revision

---
+++ sljit_src/sljitLir.c
@@ -0,0 +1,324 @@
+/*
+ *    Stack-less Just-In-Time compiler
+ *    Copyright (c) Zoltan Herczeg
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ */
+
+#include "sljitLir.h"
+
+#define FUNCTION_ENTRY() \
+	SLJIT_ASSERT(compiler->error == SLJIT_NO_ERROR)
+
+#define TEST_MEM_ERROR(ptr) \
+	if (!(ptr)) { \
+		compiler->error = SLJIT_MEMORY_ERROR; \
+		return SLJIT_MEMORY_ERROR; \
+	}
+
+#define TEST_MEM_ERROR2(ptr) \
+	if (!(ptr)) { \
+		compiler->error = SLJIT_MEMORY_ERROR; \
+		return NULL; \
+	}
+
+#define BUF_SIZE	2048
+#define ABUF_SIZE	512
+
+// Jump flags
+#define JUMP_LABEL	0x1
+#define JUMP_ADDR	0x2
+
+#ifdef SLJIT_CONFIG_X86_32
+#define PATCH_MB	0x4
+#define PATCH_MW	0x8
+#endif
+
+// ---------------------------------------------------------------------
+//  Public functions
+// ---------------------------------------------------------------------
+
+struct sljit_compiler* sljit_create_compiler(void)
+{
+	struct sljit_compiler *compiler = SLJIT_MALLOC(sizeof(struct sljit_compiler));
+
+	if (!compiler)
+		return NULL;
+
+	compiler->error = SLJIT_NO_ERROR;
+
+	compiler->labels = NULL;
+	compiler->jumps = NULL;
+	compiler->consts = NULL;
+	compiler->last_label = NULL;
+	compiler->last_jump = NULL;
+	compiler->last_const = NULL;
+
+	compiler->buf = SLJIT_MALLOC(BUF_SIZE);
+	compiler->abuf = SLJIT_MALLOC(ABUF_SIZE);
+
+	if (!compiler->buf || !compiler->abuf) {
+		if (compiler->buf)
+			SLJIT_FREE(compiler->buf);
+		if (compiler->abuf)
+			SLJIT_FREE(compiler->abuf);
+		SLJIT_FREE(compiler);
+		return NULL;
+	}
+
+	compiler->buf->next = NULL;
+	compiler->buf->used_size = 0;
+	compiler->abuf->next = NULL;
+	compiler->abuf->used_size = 0;
+
+	compiler->general = -1;
+	compiler->size = 0;
+
+#ifdef SLJIT_VERBOSE
+	compiler->verbose = NULL;
+#endif
+	return compiler;
+}
+
+void sljit_free_compiler(struct sljit_compiler *compiler)
+{
+	struct sljit_memory_fragment *buf;
+	struct sljit_memory_fragment *curr;
+
+	buf = compiler->buf;
+	while (buf) {
+		curr = buf;
+		buf = buf->next;
+		SLJIT_FREE(curr);
+	}
+
+	buf = compiler->abuf;
+	while (buf) {
+		curr = buf;
+		buf = buf->next;
+		SLJIT_FREE(curr);
+	}
+
+	SLJIT_FREE(compiler);
+}
+
+void sljit_set_label(struct sljit_jump *jump, struct sljit_label* label)
+{
+	jump->flags &= ~JUMP_ADDR;
+	jump->flags |= JUMP_LABEL;
+	jump->label = label;
+}
+
+void sljit_set_target(struct sljit_jump *jump, sljit_uw target)
+{
+	SLJIT_ASSERT(jump->flags & SLJIT_LONG_JUMP);
+
+	jump->flags &= ~JUMP_LABEL;
+	jump->flags |= JUMP_ADDR;
+	jump->target = target;
+}
+
+// ---------------------------------------------------------------------
+//  Private functions
+// ---------------------------------------------------------------------
+
+static void* ensure_buf(struct sljit_compiler *compiler, int size)
+{
+	sljit_ub *ret;
+	struct sljit_memory_fragment *new_frag;
+
+	if (compiler->buf->used_size + size <= (BUF_SIZE - sizeof(int) - sizeof(void*))) {
+		ret = compiler->buf->memory + compiler->buf->used_size;
+		compiler->buf->used_size += size;
+		return ret;
+	}
+	new_frag = SLJIT_MALLOC(BUF_SIZE);
+	if (!new_frag)
+		return NULL;
+	new_frag->next = compiler->buf;
+	compiler->buf = new_frag;
+	new_frag->used_size = size;
+	return new_frag->memory;
+}
+
+static void* ensure_abuf(struct sljit_compiler *compiler, int size)
+{
+	sljit_ub *ret;
+	struct sljit_memory_fragment *new_frag;
+
+	if (compiler->abuf->used_size + size <= (ABUF_SIZE - sizeof(int) - sizeof(void*))) {
+		ret = compiler->abuf->memory + compiler->abuf->used_size;
+		compiler->abuf->used_size += size;
+		return ret;
+	}
+	new_frag = SLJIT_MALLOC(ABUF_SIZE);
+	if (!new_frag)
+		return NULL;
+	new_frag->next = compiler->abuf;
+	compiler->abuf = new_frag;
+	new_frag->used_size = size;
+	return new_frag->memory;
+}
+
+static void reverse_buf(struct sljit_compiler *compiler)
+{
+	struct sljit_memory_fragment *buf = compiler->buf;
+	struct sljit_memory_fragment *prev = NULL;
+	struct sljit_memory_fragment *tmp;
+
+	do {
+		tmp = buf->next;
+		buf->next = prev;
+		prev = buf;
+		buf = tmp;
+	} while (buf != NULL);
+
+	compiler->buf = prev;
+}
+
+#ifdef SLJIT_DEBUG
+#define FUNCTION_CHECK_SRC(p, i) \
+	if ((p) >= SLJIT_TEMPORARY_REG1 && (p) <= SLJIT_GENERAL_REG3) \
+		SLJIT_ASSERT(i == 0); \
+	else if ((p) == SLJIT_IMM) \
+		; \
+	else if ((p) & SLJIT_LOAD_FLAG) { \
+		SLJIT_ASSERT(((p) & 0xf) <= SLJIT_GENERAL_REG3); \
+		if (((p) & 0xf) != 0) \
+			SLJIT_ASSERT((((p) >> 4) & 0xf) <= SLJIT_GENERAL_REG3); \
+		else \
+			SLJIT_ASSERT((((p) >> 4) & 0xf) == 0); \
+		SLJIT_ASSERT(((p) >> 9) == 0); \
+	} \
+	else \
+		SLJIT_ASSERT_IMPOSSIBLE();
+
+#define FUNCTION_CHECK_DST(p, i) \
+	if ((p) >= SLJIT_NO_REG && (p) <= SLJIT_GENERAL_REG3) \
+		SLJIT_ASSERT(i == 0); \
+	else if ((p) & SLJIT_LOAD_FLAG) { \
+		SLJIT_ASSERT(((p) & 0xf) <= SLJIT_GENERAL_REG3); \
+		if (((p) & 0xf) != 0) \
+			SLJIT_ASSERT((((p) >> 4) & 0xf) <= SLJIT_GENERAL_REG3); \
+		else \
+			SLJIT_ASSERT((((p) >> 4) & 0xf) == 0); \
+		SLJIT_ASSERT(((p) >> 9) == 0); \
+	} \
+	else \
+		SLJIT_ASSERT_IMPOSSIBLE();
+#endif
+
+#ifdef SLJIT_VERBOSE
+
+void sljit_compiler_verbose(struct sljit_compiler *compiler, FILE* verbose)
+{
+	compiler->verbose = verbose;
+}
+
+static char* reg_names[] = {
+	"<noreg>", "tmp_r1", "tmp_r2", "tmp_r3", "gen_r1", "gen_r2", "gen_r3"
+};
+
+#define sljit_emit_enter_verbose() \
+	if (compiler->verbose) fprintf(compiler->verbose, "  enter %d %d\n", args, general);
+#define sljit_emit_return_verbose() \
+	if (compiler->verbose) fprintf(compiler->verbose, "  return %s\n", reg_names[reg]);
+#define sljit_verbose_param(p, i) \
+	if ((p) & SLJIT_IMM_FLAG) \
+		fprintf(compiler->verbose, "#%d", (i)); \
+	else if ((p) & SLJIT_LOAD_FLAG) { \
+		if ((p) & 0xF) { \
+			if ((i) != 0) { \
+				if (((p) >> 4) & 0xF) \
+					fprintf(compiler->verbose, "[%s + %s + #%d]", reg_names[(p) & 0xF], reg_names[((p) >> 4)& 0xF], (i)); \
+				else \
+					fprintf(compiler->verbose, "[%s + #%d]", reg_names[(p) & 0xF], (i)); \
+			} \
+			else { \
+				if (((p) >> 4) & 0xF) \
+					fprintf(compiler->verbose, "[%s + %s]", reg_names[(p) & 0xF], reg_names[((p) >> 4)& 0xF]); \
+				else \
+					fprintf(compiler->verbose, "[%s]", reg_names[(p) & 0xF]); \
+			} \
+		} \
+		else \
+			fprintf(compiler->verbose, "[#%d]", (i)); \
+	} else \
+		fprintf(compiler->verbose, "%s", reg_names[p]);
+static char* op1_names[] = {
+	"mov", "not", "neg"
+};
+#define sljit_emit_op1_verbose() \
+	if (compiler->verbose) { \
+		fprintf(compiler->verbose, "  %s ", op1_names[op]); \
+		sljit_verbose_param(dst, dstw); \
+		fprintf(compiler->verbose, ", "); \
+		sljit_verbose_param(src, srcw); \
+		fprintf(compiler->verbose, "\n"); \
+	}
+static char* op2_names[] = {
+	"add", "addc", "sub", "subc", "mul",
+	"and", "or", "xor", "shl", "lshr", "ashr"
+};
+#define sljit_emit_op2_verbose() \
+	if (compiler->verbose) { \
+		fprintf(compiler->verbose, "  %s ", op2_names[op]); \
+		sljit_verbose_param(dst, dstw); \
+		fprintf(compiler->verbose, ", "); \
+		sljit_verbose_param(src1, src1w); \
+		fprintf(compiler->verbose, ", "); \
+		sljit_verbose_param(src2, src2w); \
+		fprintf(compiler->verbose, "\n"); \
+	}
+#define sljit_emit_label_verbose() \
+	if (compiler->verbose) fprintf(compiler->verbose, "label:\n");
+static char* jump_names[] = {
+	"c_equal", "c_not_equal",
+	"c_less", "c_not_less",
+	"c_greater", "c_not_greater",
+	"c_sig_less", "c_sig_not_less",
+	"c_sig_greater", "c_sig_not_greater",
+	"c_carry", "c_not_carry",
+	"c_zero", "c_not_zero",
+	"c_overflow", "c_not_overflow",
+	"jump",
+};
+#define sljit_emit_jump_verbose() \
+	if (compiler->verbose) fprintf(compiler->verbose, "  %s %s\n", jump_names[type & 0xff], (type & SLJIT_LONG_JUMP) ? "(long)" : "");
+#define sljit_emit_set_cond_verbose() \
+	if (compiler->verbose) { \
+		fprintf(compiler->verbose, "  cond_set "); \
+		sljit_verbose_param(dst, dstw); \
+		fprintf(compiler->verbose, ", %s\n", jump_names[type]); \
+	}
+
+#else
+
+#define sljit_emit_enter_verbose()
+#define sljit_emit_return_verbose()
+#define sljit_emit_op1_verbose()
+#define sljit_emit_op2_verbose()
+#define sljit_emit_label_verbose()
+#define sljit_emit_jump_verbose()
+#define sljit_emit_set_cond_verbose()
+
+#endif
+
+// ---------------------------------------------------------------------
+//  Arch dependent
+// ---------------------------------------------------------------------
+
+#if defined(SLJIT_CONFIG_X86_32)
+	#include "sljitNativeX86_32.c"
+#else
+	#error "Unknown native code generation method"
+#endif
+
+
---
+++ sljit_src/sljitLir.h
@@ -0,0 +1,335 @@
+/*
+ *    Stack-less Just-In-Time compiler
+ *    Copyright (c) Zoltan Herczeg
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ */
+
+#ifndef _SLJIT_H_
+#define _SLJIT_H_
+
+// ---------------------------------------------------------------------
+//  Stack-Less JIT compiler for multiple platforms (x86, ARM, powerPC)
+// ---------------------------------------------------------------------
+//
+// Short description of the general approach
+//  Advantages:
+//    - The execution can be continued from any LIR instruction
+//      In other words, jump into and out of the code is safe
+//    - Target of (conditional) jump and call instructions can be
+//      dynamically modified during the execution of the code
+//    - Constants can be modified during the execution of the code
+//  Disadvantages:
+//    - Limited number of registers (only 6, max 3 temporary and max 3 general)
+//    - Variables cannot be stored temporarily on the stack
+//  In practice:
+//    - This approach is very effective for interpreters
+//      - One of the general registers tipically points to a stack interface
+//      - It can jump to any exception handler anytime (even for another
+//        function. It is safe for SLJIT.)
+//      - Fast paths can be modified runtime reflecting the fastest execution
+//        way of the dynamic language
+//      - SLJIT supports complex memory addressing modes
+//    - Optimizations (later)
+//      - Only for basic blocks
+
+// ---------------------------------------------------------------------
+//  Configuration
+// ---------------------------------------------------------------------
+
+// Architecture selection
+#define SLJIT_CONFIG_X86_32
+
+// General libraries
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define SLJIT_MALLOC(size) malloc(size)
+#define SLJIT_FREE(ptr) free(ptr)
+
+#define SLJIT_MEMMOVE(dest, src, len) memmove(dest, src, len)
+
+// Debug
+#define SLJIT_DEBUG
+
+// Verbose operations
+#define SLJIT_VERBOSE
+
+#define INLINE __inline
+
+// Byte structure
+typedef unsigned char sljit_ub;
+typedef char sljit_b;
+// Machine word structure. Can encapsulate a pointer.
+//   32 bit for 32 bit machines
+//   64 bit for 64 bit machines
+typedef unsigned int sljit_uw;
+typedef int sljit_w;
+
+// Machine independent types
+typedef unsigned short sljit_u16b;
+typedef short sljit_16b;
+typedef unsigned int sljit_u32b;
+typedef int sljit_32b;
+
+#ifdef SLJIT_DEBUG
+#define SLJIT_ASSERT(x) \
+	do { \
+		if (!(x)) { \
+			printf("Assertion failed at " __FILE__ ":%d\n", __LINE__); \
+			*((int*)0) = 0; \
+		} \
+	} while (0)
+#define SLJIT_ASSERT_IMPOSSIBLE() \
+	do { \
+		printf("Should never been reached " __FILE__ ":%d\n", __LINE__); \
+		*((int*)0) = 0; \
+	} while (0)
+#else
+#define SLJIT_ASSERT(x) \
+	do { } while (0)
+#define SLJIT_ASSERT_IMPOSSIBLE() \
+	do { } while (0)
+#endif
+
+// ABI (Application Binary Interface) types
+#ifdef __GNUC__
+#define SLJIT_CDECL __attribute__ ((cdecl))
+#else
+#define SLJIT_CDECL __cdecl
+#endif
+
+#ifdef __GNUC__
+#define SLJIT_FASTCALL __attribute__ ((fastcall))
+#else
+#define SLJIT_FASTCALL __fastcall
+#endif
+
+// ---------------------------------------------------------------------
+//  Error codes
+// ---------------------------------------------------------------------
+
+#define SLJIT_NO_ERROR		0
+#define SLJIT_CODE_GENERATED	1
+#define SLJIT_MEMORY_ERROR	2
+
+// ---------------------------------------------------------------------
+//  Registers
+// ---------------------------------------------------------------------
+
+#define SLJIT_NO_REG		0
+
+#define SLJIT_TEMPORARY_REG1	1
+#define SLJIT_TEMPORARY_REG2	2
+#define SLJIT_TEMPORARY_REG3	3
+
+#define SLJIT_GENERAL_REG1	4
+#define SLJIT_GENERAL_REG2	5
+#define SLJIT_GENERAL_REG3	6
+
+#define SLJIT_NO_TMP_REGISTERS	3
+#define SLJIT_NO_GEN_REGISTERS	3
+#define SLJIT_NO_REGISTERS	6
+
+// Return with machine word or double machine word
+
+#define SLJIT_PREF_RET_REG	SLJIT_TEMPORARY_REG1
+#define SLJIT_PREF_RET_HIREG	SLJIT_TEMPORARY_REG3
+
+// x86 prefers temporary registers for special purposes. If not these
+// registers are used, it costs a little performance drawback. It doeasn't
+// matter for other archs
+
+#define SLJIT_PREF_MUL_BASE	SLJIT_TEMPORARY_REG1
+#define SLJIT_PREF_MUL_WITH	SLJIT_TEMPORARY_REG3
+#define SLJIT_PREF_SHIFT_REG	SLJIT_TEMPORARY_REG2
+
+// ---------------------------------------------------------------------
+//  Main functions
+// ---------------------------------------------------------------------
+
+struct sljit_memory_fragment {
+	struct sljit_memory_fragment *next;
+	int used_size;
+	sljit_ub memory[1];
+};
+
+struct sljit_label {
+	struct sljit_label *next;
+	sljit_uw addr;
+	int size;
+};
+
+struct sljit_jump {
+	struct sljit_jump *next;
+	int flags;
+	sljit_uw addr;
+	union {
+		sljit_uw target;
+		struct sljit_label* label;
+	};
+};
+
+struct sljit_const {
+	struct sljit_consts *next;
+	sljit_uw addr;
+};
+
+struct sljit_compiler {
+	int error;
+
+	struct sljit_label *labels;
+	struct sljit_jump *jumps;
+	struct sljit_const *consts;
+	struct sljit_label *last_label;
+	struct sljit_jump *last_jump;
+	struct sljit_const *last_const;
+
+	struct sljit_memory_fragment *buf;
+	struct sljit_memory_fragment *abuf;
+
+	// Used general registers
+	int general;
+	int size;
+
+#ifdef SLJIT_VERBOSE
+	FILE* verbose;
+#endif
+};
+
+// Creates an sljit compiler.
+// Returns NULL if failed
+struct sljit_compiler* sljit_create_compiler(void);
+// Free everything except the codes
+void sljit_free_compiler(struct sljit_compiler *compiler);
+
+#ifdef SLJIT_VERBOSE
+// NULL = no verbose
+void sljit_compiler_verbose(struct sljit_compiler *compiler, FILE* verbose);
+#endif
+
+void* sljit_generate_code(struct sljit_compiler *compiler);
+void sljit_free_code(void* code);
+
+// Instruction generation. Returns with error code
+
+// Entry instruction. The instruction has "args" number of arguments
+// and will use the first "general" number of general registers.
+// Therefore, "args" must be less or equal than "general"
+
+#define CALL_TYPE_CDECL		0
+#define CALL_TYPE_FASTCALL	1
+
+int sljit_emit_enter(struct sljit_compiler *compiler, int type, int args, int general);
+
+// Return from jit. Return with NO_REG or a register
+int sljit_emit_return(struct sljit_compiler *compiler, int reg);
+
+// Source and destination values for arithmetical instructions
+//  imm           - a simple immediate value (cannot be destination)
+//  reg           - any of the registers (immediate argument unused)
+//  [imm]         - absolute immediate memory address
+//  [reg+imm]     - indirect memory address
+//  [reg+reg+imm] - two level indirect addressing
+
+#define SLJIT_LOAD_FLAG		0x100
+#define SLJIT_IMM_FLAG		0x200
+
+// Helper defines
+// Register output: simply the name of the register
+// For destination, you can use SLJIT_NO_REG as well
+#define SLJIT_IMM		SLJIT_IMM_FLAG
+#define SLJIT_MEM0()		SLJIT_LOAD_FLAG
+#define SLJIT_MEM1(r1)		SLJIT_LOAD_FLAG | (r1)
+#define SLJIT_MEM2(r1, r2)	SLJIT_LOAD_FLAG | (r1) | ((r2) << 4)
+
+// Most of the following instructions are also set the CPU status-flags
+// Common flags for all architectures (x86, ARM, PPC)
+//  - carry flag
+//  - overflow flag
+//  - zero flag
+//  - negative/positive flag (depends on arc)
+// Note:
+//  - instructions which are not set status flags are marked with asterisk (*)
+//  - no perf penalty for setting the CPU status flags
+//  - you cannot postpone conditional jump instructions depending on
+//    one of these instructions
+
+// * - CPU flags not set
+#define SLJIT_MOV			0
+// * - CPU flags not set
+#define SLJIT_NOT			1
+#define SLJIT_NEG			2
+
+int sljit_emit_op1(struct sljit_compiler *compiler, int op,
+	int dst, sljit_w dstw,
+	int src, sljit_w srcw);
+
+#define SLJIT_ADD			0
+#define SLJIT_ADDC			1
+#define SLJIT_SUB			2
+#define SLJIT_SUBC			3
+#define SLJIT_MUL			4
+#define SLJIT_AND			5
+#define SLJIT_OR			6
+#define SLJIT_XOR			7
+#define SLJIT_SHL			8
+#define SLJIT_LSHR			9
+#define SLJIT_ASHR			10
+
+int sljit_emit_op2(struct sljit_compiler *compiler, int op,
+	int dst, sljit_w dstw,
+	int src1, sljit_w src1w,
+	int src2, sljit_w src2w);
+
+// Label and jump instructions
+
+struct sljit_label* sljit_emit_label(struct sljit_compiler *compiler);
+
+// Invert conditional instruction: xor (^) with 0x1
+#define SLJIT_C_EQUAL			0
+#define SLJIT_C_NOT_EQUAL		1
+#define SLJIT_C_LESS			2
+#define SLJIT_C_NOT_LESS		3
+#define SLJIT_C_GREATER			4
+#define SLJIT_C_NOT_GREATER		5
+#define SLJIT_C_SIG_LESS		6
+#define SLJIT_C_SIG_NOT_LESS		7
+#define SLJIT_C_SIG_GREATER		8
+#define SLJIT_C_SIG_NOT_GREATER	9
+
+#define SLJIT_C_CARRY			10
+#define SLJIT_C_NOT_CARRY		11
+#define SLJIT_C_ZERO			12
+#define SLJIT_C_NOT_ZERO		13
+#define SLJIT_C_OVERFLOW		14
+#define SLJIT_C_NOT_OVERFLOW		15
+
+#define SLJIT_JUMP			16
+
+// The target can be redefined later
+#define SLJIT_LONG_JUMP			0x100
+
+struct sljit_jump* sljit_emit_jump(struct sljit_compiler *compiler, int type);
+void sljit_set_label(struct sljit_jump *jump, struct sljit_label* label);
+void sljit_set_target(struct sljit_jump *jump, sljit_uw target);
+
+int sljit_emit_cond_set(struct sljit_compiler *compiler, int dst, sljit_w dstw, int type);
+
+// After the code generation the address for label, jump and const instructions
+// are computed. These addresses must be saved elsewhere if we plan to use them
+// later after sljitFreeCompiler
+#define sljit_get_label_addr(label)	(label->addr)
+#define sljit_get_jump_addr(jump)	(jump->addr)
+#define sljit_get_const_addr(cons)	(cons->addr)
+
+#endif
+
