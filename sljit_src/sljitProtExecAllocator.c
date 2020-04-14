zherczeg/sljit

commit 9a3d3998dcaf151f5c562f8dde457790fe0d167c

netbsd: implement non file backed alternative for sljitProtExecAllocator (#29)
the experimental sljitProtExecAllocator uses file backed maps to
workaround W^X policies and is therefore incompatible with fork()

NetBSD allows anonymous pages to have multiple maps with different
permissions by using mremap with the MAP_REMAPDUP flag so use that
instead.

sljit_src/sljitProtExecAllocator.c
@@ -96,10 +96,9 @@ struct chunk_header {
 #endif
 #endif
 
+#if !(defined(__NetBSD__) && defined(MAP_REMAPDUP))
 int mkostemp(char *template, int flags);
-#if !defined(__NetBSD__)
 char *secure_getenv(const char *name);
-#endif
 
 static SLJIT_INLINE int create_tempfile(void)
 {
@@ -204,6 +203,36 @@ static SLJIT_INLINE struct chunk_header* alloc_chunk(sljit_uw size)
 	retval->fd = fd;
 	return retval;
 }
+#else
+static SLJIT_INLINE struct chunk_header* alloc_chunk(sljit_uw size)
+{
+	struct chunk_header *retval;
+	void *maprx;
+
+	retval = (struct chunk_header *)mmap(NULL, size,
+			PROT_MPROTECT(PROT_EXEC|PROT_WRITE|PROT_READ),
+			MAP_ANON, -1, 0);
+
+	if (retval == MAP_FAILED)
+		return NULL;
+
+	maprx = mremap(retval, size, NULL, size, MAP_REMAPDUP);
+	if (maprx == MAP_FAILED) {
+		munmap((void *)retval, size);
+		return NULL;
+	}
+
+	if (mprotect(retval, size, PROT_READ | PROT_WRITE) == -1 ||
+		mprotect(maprx, size, PROT_READ | PROT_EXEC) == -1) {
+		munmap(maprx, size);
+		munmap((void *)retval, size);
+		return NULL;
+	}
+	retval->executable = maprx;
+	retval->fd = -1;
+	return retval;
+}
+#endif
 
 static SLJIT_INLINE void free_chunk(void *chunk, sljit_uw size)
 {
@@ -212,7 +241,8 @@ static SLJIT_INLINE void free_chunk(void *chunk, sljit_uw size)
 	int fd = header->fd;
 	munmap(header->executable, size);
 	munmap((void *)header, size);
-	close(fd);
+	if (fd != -1)
+		close(fd);
 }
 
 /* --------------------------------------------------------------------- */

commit 4b2ecbec97d0048db8ce500ca983457bfac9b839

netbsd: cleanup alloc_chunk() in sljitProtExec for NetBSD >= 8
in line with the current semantics make the map SHARED so it could
be used (with the same limitations and risks) like the old function
after fork()

Fixes: 9a3d3998dcaf151f5c562f8dde457790fe0d167c

sljit_src/sljitProtExecAllocator.c
@@ -220,28 +220,26 @@ static SLJIT_INLINE struct chunk_header* alloc_chunk(sljit_uw size)
 static SLJIT_INLINE struct chunk_header* alloc_chunk(sljit_uw size)
 {
 	struct chunk_header *retval;
-	void *maprx;
 
 	retval = (struct chunk_header *)mmap(NULL, size,
-			PROT_MPROTECT(PROT_EXEC|PROT_WRITE|PROT_READ),
-			MAP_ANON, -1, 0);
+			PROT_READ | PROT_WRITE | PROT_MPROTECT(PROT_EXEC),
+			MAP_ANON | MAP_SHARED, -1, 0);
 
 	if (retval == MAP_FAILED)
 		return NULL;
 
-	maprx = mremap(retval, size, NULL, size, MAP_REMAPDUP);
-	if (maprx == MAP_FAILED) {
+	retval->executable = mremap(retval, size, NULL, size, MAP_REMAPDUP);
+	if (retval->executable == MAP_FAILED) {
 		munmap((void *)retval, size);
 		return NULL;
 	}
 
-	if (mprotect(retval, size, PROT_READ | PROT_WRITE) == -1 ||
-		mprotect(maprx, size, PROT_READ | PROT_EXEC) == -1) {
-		munmap(maprx, size);
+	if (mprotect(retval->executable, size, PROT_READ | PROT_EXEC) == -1) {
+		munmap(retval->executable, size);
 		munmap((void *)retval, size);
 		return NULL;
 	}
-	retval->executable = maprx;
+
 	return retval;
 }
 #endif /* NetBSD >= 8 */