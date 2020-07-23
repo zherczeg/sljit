zherczeg/sljit

commit fcf394a54711946ff938c084d8123e4af9940db2

Fix doc/tutorial examples and improve Makefile rules (#54)
* Fix use of out-of-date APIs in tutorials and typo

* Tutorial examples no longer compiled due to API breakages. Use
  updated call APIs and argument types to have these examples work
  correctly again.
* Update array_access.c to use array length instead of uninitialized S1
  value for loop termination condition. Also initialize all array elements
  explicitly.
* Fix small typo in sljitLir.h

* Update Makefile rules and add `examples` target

* Remove explicit $(BINDIR) directory dependency to ensure that
  targets don't rebuild uneccessarily.
* Add targets for all doc/tutorial examples. All of these targets
  can be built via the `examples` target:
  > make examples

* Fix wording of alignment note

Makefile
@@ -20,12 +20,14 @@ CFLAGS += -O2 -Wall
 REGEX_CFLAGS += $(CFLAGS) -fshort-wchar
 LDFLAGS = $(EXTRA_LDFLAGS)
 
-TARGET = sljit_test regex_test
-
 BINDIR = bin
 SRCDIR = sljit_src
 TESTDIR = test_src
 REGEXDIR = regex_src
+EXAMPLEDIR = doc/tutorial
+
+TARGET = $(BINDIR)/sljit_test $(BINDIR)/regex_test
+EXAMPLE_TARGET = $(BINDIR)/func_call $(BINDIR)/first_program $(BINDIR)/branch $(BINDIR)/loop $(BINDIR)/array_access $(BINDIR)/func_call $(BINDIR)/struct_access $(BINDIR)/temp_var $(BINDIR)/brainfuck
 
 SLJIT_HEADERS = $(SRCDIR)/sljitLir.h $(SRCDIR)/sljitConfig.h $(SRCDIR)/sljitConfigInternal.h
 
@@ -37,31 +39,60 @@ SLJIT_LIR_FILES = $(SRCDIR)/sljitLir.c $(SRCDIR)/sljitExecAllocator.c $(SRCDIR)/
 	$(SRCDIR)/sljitNativeTILEGX_64.c \
 	$(SRCDIR)/sljitNativeX86_common.c $(SRCDIR)/sljitNativeX86_32.c $(SRCDIR)/sljitNativeX86_64.c
 
-all: $(BINDIR) $(TARGET)
+.PHONY: all examples
+
+all: $(TARGET)
 
 $(BINDIR) :
 	mkdir $(BINDIR)
 
-$(BINDIR)/sljitLir.o : $(BINDIR) $(SLJIT_LIR_FILES) $(SLJIT_HEADERS)
+$(BINDIR)/sljitLir.o : $(SLJIT_LIR_FILES) $(SLJIT_HEADERS)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c -o $@ $(SRCDIR)/sljitLir.c
 
-$(BINDIR)/sljitMain.o : $(TESTDIR)/sljitMain.c $(BINDIR) $(SLJIT_HEADERS)
+$(BINDIR)/sljitMain.o : $(TESTDIR)/sljitMain.c $(SLJIT_HEADERS)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c -o $@ $(TESTDIR)/sljitMain.c
 
-$(BINDIR)/sljitTest.o : $(TESTDIR)/sljitTest.c $(BINDIR) $(SLJIT_HEADERS)
+$(BINDIR)/sljitTest.o : $(TESTDIR)/sljitTest.c $(SLJIT_HEADERS)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c -o $@ $(TESTDIR)/sljitTest.c
 
-$(BINDIR)/regexMain.o : $(REGEXDIR)/regexMain.c $(BINDIR) $(SLJIT_HEADERS)
+$(BINDIR)/regexMain.o : $(REGEXDIR)/regexMain.c $(SLJIT_HEADERS)
 	$(CC) $(CPPFLAGS) $(REGEX_CFLAGS) -c -o $@ $(REGEXDIR)/regexMain.c
 
-$(BINDIR)/regexJIT.o : $(REGEXDIR)/regexJIT.c $(BINDIR) $(SLJIT_HEADERS) $(REGEXDIR)/regexJIT.h
+$(BINDIR)/regexJIT.o : $(REGEXDIR)/regexJIT.c $(SLJIT_HEADERS) $(REGEXDIR)/regexJIT.h
 	$(CC) $(CPPFLAGS) $(REGEX_CFLAGS) -c -o $@ $(REGEXDIR)/regexJIT.c
 
 clean:
-	rm -f $(BINDIR)/*.o $(BINDIR)/sljit_test $(BINDIR)/regex_test
+	-$(RM) -f $(BINDIR)/*.o $(BINDIR)/sljit_test $(BINDIR)/regex_test $(EXAMPLE_TARGET)
 
-sljit_test: $(BINDIR)/sljitMain.o $(BINDIR)/sljitTest.o $(BINDIR)/sljitLir.o
-	$(CC) $(CFLAGS) $(LDFLAGS) $(BINDIR)/sljitMain.o $(BINDIR)/sljitTest.o $(BINDIR)/sljitLir.o -o $(BINDIR)/$@ -lm -lpthread
+$(BINDIR)/sljit_test: $(BINDIR)/sljitMain.o $(BINDIR)/sljitTest.o $(BINDIR)/sljitLir.o
+	$(CC) $(CFLAGS) $(LDFLAGS) $(BINDIR)/sljitMain.o $(BINDIR)/sljitTest.o $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
 
-regex_test: $(BINDIR)/regexMain.o $(BINDIR)/regexJIT.o $(BINDIR)/sljitLir.o
-	$(CC) $(CFLAGS) $(LDFLAGS) $(BINDIR)/regexMain.o $(BINDIR)/regexJIT.o $(BINDIR)/sljitLir.o -o $(BINDIR)/$@ -lm -lpthread
+$(BINDIR)/regex_test: $(BINDIR)/regexMain.o $(BINDIR)/regexJIT.o $(BINDIR)/sljitLir.o
+	$(CC) $(CFLAGS) $(LDFLAGS) $(BINDIR)/regexMain.o $(BINDIR)/regexJIT.o $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
+
+
+examples: $(EXAMPLE_TARGET)
+
+$(BINDIR)/first_program: $(EXAMPLEDIR)/first_program.c $(BINDIR)/sljitLir.o
+	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/first_program.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
+
+$(BINDIR)/branch: $(EXAMPLEDIR)/branch.c $(BINDIR)/sljitLir.o
+	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/branch.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
+
+$(BINDIR)/loop: $(EXAMPLEDIR)/loop.c $(BINDIR)/sljitLir.o
+	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/loop.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
+
+$(BINDIR)/array_access: $(EXAMPLEDIR)/array_access.c $(BINDIR)/sljitLir.o
+	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/array_access.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
+
+$(BINDIR)/func_call: $(EXAMPLEDIR)/func_call.c $(BINDIR)/sljitLir.o
+	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/func_call.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
+
+$(BINDIR)/struct_access: $(EXAMPLEDIR)/struct_access.c $(BINDIR)/sljitLir.o
+	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/struct_access.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
+
+$(BINDIR)/temp_var: $(EXAMPLEDIR)/temp_var.c $(BINDIR)/sljitLir.o
+	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/temp_var.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
+
+$(BINDIR)/brainfuck: $(EXAMPLEDIR)/brainfuck.c $(BINDIR)/sljitLir.o
+	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/brainfuck.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread

doc/tutorial/array_access.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef long SLJIT_CALL (*func_arr_t)(long *arr, long narr);
+typedef SLJIT_FUNC long (*func_arr_t)(long *arr, long narr);
 
-static long SLJIT_CALL print_num(long a)
+static SLJIT_FUNC long print_num(long a)
 {
 	printf("num = %ld\n", a);
 	return a + 1;
@@ -31,16 +31,17 @@ static int array_access(long *arr, long narr)
 	func_arr_t func;
 
 	/* Create a SLJIT compiler */
-	struct sljit_compiler *C = sljit_create_compiler();
+	struct sljit_compiler *C = sljit_create_compiler(NULL);
 
-	sljit_emit_enter(C, 0,  2,  1, 3, 0, 0, 0);
+	sljit_emit_enter(C, 0, SLJIT_ARG1(SW),  1, 3, 0, 0, 0);
 	/*                  opt arg R  S  FR FS local_size */
 	sljit_emit_op2(C, SLJIT_XOR, SLJIT_S2, 0, SLJIT_S2, 0, SLJIT_S2, 0);				// S2 = 0
+	sljit_emit_op1(C, SLJIT_MOV, SLJIT_S1, 0, SLJIT_IMM, narr);                         // S1 = narr
 	struct sljit_label *loopstart = sljit_emit_label(C);								// loopstart:
 	struct sljit_jump *out = sljit_emit_cmp(C, SLJIT_GREATER_EQUAL, SLJIT_S2, 0, SLJIT_S1, 0);	// S2 >= a --> jump out
 
 	sljit_emit_op1(C, SLJIT_MOV, SLJIT_R0, 0, SLJIT_MEM2(SLJIT_S0, SLJIT_S2), SLJIT_WORD_SHIFT);// R0 = (long *)S0[S2];
-	sljit_emit_ijump(C, SLJIT_CALL1, SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));					// print_num(R0);
+	sljit_emit_icall(C, SLJIT_CALL, SLJIT_RET(SW) | SLJIT_ARG1(SW), SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));					// print_num(R0);
 
 	sljit_emit_op2(C, SLJIT_ADD, SLJIT_S2, 0, SLJIT_S2, 0, SLJIT_IMM, 1);						// S2 += 1
 	sljit_set_label(sljit_emit_jump(C, SLJIT_JUMP), loopstart);									// jump loopstart
@@ -65,6 +66,6 @@ static int array_access(long *arr, long narr)
 
 int main()
 {
-	long arr[8] = { 3, -10, 4, 6, 8, 12, 2000 };
+	long arr[8] = { 3, -10, 4, 6, 8, 12, 2000, 0 };
 	return array_access(arr, 8);
 }

doc/tutorial/brainfuck.c
@@ -89,7 +89,7 @@ struct loop_node_st {
 static struct loop_node_st loop_stack[BF_LOOP_LEVEL];
 static int loop_sp;
 
-static int loop_push(struct sljit_label *loop_start, struct sljit_jump *loop_end)
+static SLJIT_FUNC int loop_push(struct sljit_label *loop_start, struct sljit_jump *loop_end)
 {
 	if (loop_sp >= BF_LOOP_LEVEL)
 		return -1;
@@ -100,7 +100,7 @@ static int loop_push(struct sljit_label *loop_start, struct sljit_jump *loop_end
 	return 0;
 }
 
-static int loop_pop(struct sljit_label **loop_start, struct sljit_jump **loop_end)
+static SLJIT_FUNC int loop_pop(struct sljit_label **loop_start, struct sljit_jump **loop_end)
 {
 	if (loop_sp <= 0)
 		return -1;
@@ -111,22 +111,22 @@ static int loop_pop(struct sljit_label **loop_start, struct sljit_jump **loop_en
 	return 0;
 }
 
-static SLJIT_CALL void *my_alloc(long size, long n)
+static SLJIT_FUNC void *my_alloc(long size, long n)
 {
 	return calloc(size, n);
 }
 
-static SLJIT_CALL void my_putchar(long c)
+static SLJIT_FUNC void my_putchar(long c)
 {
 	putchar(c);
 }
 
-static SLJIT_CALL long my_getchar(void)
+static SLJIT_FUNC long my_getchar(void)
 {
 	return getchar();
 }
 
-static SLJIT_CALL void my_free(void *mem)
+static SLJIT_FUNC void my_free(void *mem)
 {
 	free(mem);
 }
@@ -140,7 +140,7 @@ static void *compile(FILE *src, unsigned long *lcode)
 	int chr;
 	int nchr;
 
-	struct sljit_compiler *C = sljit_create_compiler();
+	struct sljit_compiler *C = sljit_create_compiler(NULL);
 	struct sljit_jump *end;
 	struct sljit_label *loop_start;
 	struct sljit_jump *loop_end;
@@ -148,13 +148,13 @@ static void *compile(FILE *src, unsigned long *lcode)
 	int SP = SLJIT_S0;			/* bf SP */
 	int CELLS = SLJIT_S1;		/* bf array */
 
-	sljit_emit_enter(C, 0,  0,  2, 2, 0, 0, 0);								/* opt arg R  S  FR FS local_size */
+	sljit_emit_enter(C, 0,  SLJIT_ARG1(SW) | SLJIT_ARG2(SW),  2, 2, 0, 0, 0);								/* opt arg R  S  FR FS local_size */
 
 	sljit_emit_op2(C, SLJIT_XOR, SP, 0, SP, 0, SP, 0);						/* SP = 0 */
 
 	sljit_emit_op1(C, SLJIT_MOV, SLJIT_R0, 0, SLJIT_IMM, BF_CELL_SIZE);
 	sljit_emit_op1(C, SLJIT_MOV, SLJIT_R1, 0, SLJIT_IMM, 1);
-	sljit_emit_ijump(C, SLJIT_CALL1, SLJIT_IMM, SLJIT_FUNC_OFFSET(my_alloc));/* calloc(BF_CELL_SIZE, 1) => R0 */
+	sljit_emit_icall(C, SLJIT_CALL, SLJIT_RET(SW) | SLJIT_ARG1(SW) | SLJIT_ARG2(SW), SLJIT_IMM, SLJIT_FUNC_OFFSET(my_alloc));/* calloc(BF_CELL_SIZE, 1) => R0 */
 
 	end = sljit_emit_cmp(C, SLJIT_EQUAL, SLJIT_R0, 0, SLJIT_IMM, 0);		/* R0 == 0 --> jump end */
 
@@ -164,10 +164,10 @@ static void *compile(FILE *src, unsigned long *lcode)
 		switch (chr) {
 		case '+':
 		case '-':
-			sljit_emit_op1(C, SLJIT_MOV_UB, SLJIT_R0, 0, SLJIT_MEM2(CELLS, SP), 0);		/* R0 = CELLS[SP] */
+			sljit_emit_op1(C, SLJIT_MOV_U8, SLJIT_R0, 0, SLJIT_MEM2(CELLS, SP), 0);		/* R0 = CELLS[SP] */
 			sljit_emit_op2(C, chr == '+' ? SLJIT_ADD : SLJIT_SUB, 
 						   SLJIT_R0, 0, SLJIT_R0, 0, SLJIT_IMM, nchr);					/* R0 ?= nchr */
-			sljit_emit_op1(C, SLJIT_MOV_UB, SLJIT_MEM2(CELLS, SP), 0, SLJIT_R0, 0);		/* CELLS[SP] = R0 */
+			sljit_emit_op1(C, SLJIT_MOV_U8, SLJIT_MEM2(CELLS, SP), 0, SLJIT_R0, 0);		/* CELLS[SP] = R0 */
 			break;
 		case '>':
 		case '<':
@@ -175,16 +175,16 @@ static void *compile(FILE *src, unsigned long *lcode)
 						   SP, 0, SP, 0, SLJIT_IMM, nchr);								/* SP ?= nchr */
 			break;
 		case '.':
-			sljit_emit_op1(C, SLJIT_MOV_UB, SLJIT_R0, 0, SLJIT_MEM2(CELLS, SP), 0);		/* R0 = CELLS[SP] */
-			sljit_emit_ijump(C, SLJIT_CALL1, SLJIT_IMM, SLJIT_FUNC_OFFSET(my_putchar));	/* putchar(R0) */
+			sljit_emit_op1(C, SLJIT_MOV_U8, SLJIT_R0, 0, SLJIT_MEM2(CELLS, SP), 0);		/* R0 = CELLS[SP] */
+			sljit_emit_icall(C, SLJIT_CALL, SLJIT_ARG1(SW), SLJIT_IMM, SLJIT_FUNC_OFFSET(my_putchar));	/* putchar(R0) */
 			break;
 		case ',':
-			sljit_emit_ijump(C, SLJIT_CALL0, SLJIT_IMM, SLJIT_FUNC_OFFSET(my_getchar));	/* R0 = getchar() */
-			sljit_emit_op1(C, SLJIT_MOV_UB, SLJIT_MEM2(CELLS, SP), 0, SLJIT_R0, 0);		/* CELLS[SP] = R0 */
+			sljit_emit_icall(C, SLJIT_CALL, SLJIT_RET(SW), SLJIT_IMM, SLJIT_FUNC_OFFSET(my_getchar));	/* R0 = getchar() */
+			sljit_emit_op1(C, SLJIT_MOV_U8, SLJIT_MEM2(CELLS, SP), 0, SLJIT_R0, 0);		/* CELLS[SP] = R0 */
 			break;
 		case '[':
 			loop_start = sljit_emit_label(C);											/* loop_start: */
-			sljit_emit_op1(C, SLJIT_MOV_UB, SLJIT_R0, 0, SLJIT_MEM2(CELLS, SP), 0);		/* R0 = CELLS[SP] */
+			sljit_emit_op1(C, SLJIT_MOV_U8, SLJIT_R0, 0, SLJIT_MEM2(CELLS, SP), 0);		/* R0 = CELLS[SP] */
 			loop_end = sljit_emit_cmp(C, SLJIT_EQUAL, SLJIT_R0, 0, SLJIT_IMM, 0);		/* IF R0 == 0 goto loop_end */
 			
 			if (loop_push(loop_start, loop_end)) {
@@ -210,7 +210,7 @@ static void *compile(FILE *src, unsigned long *lcode)
 	}
 
 	sljit_emit_op1(C, SLJIT_MOV, SLJIT_R0, 0, CELLS, 0);
-	sljit_emit_ijump(C, SLJIT_CALL1, SLJIT_IMM, SLJIT_FUNC_OFFSET(my_free));	/* free(CELLS) */
+	sljit_emit_icall(C, SLJIT_CALL, SLJIT_ARG1(SW), SLJIT_IMM, SLJIT_FUNC_OFFSET(my_free));	/* free(CELLS) */
 
 	sljit_set_label(end, sljit_emit_label(C));
 	sljit_emit_return(C, SLJIT_UNUSED, 0, 0);

doc/tutorial/branch.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef long SLJIT_CALL (*func3_t)(long a, long b, long c);
+typedef SLJIT_FUNC long (*func3_t)(long a, long b, long c);
 
 /*
   This example, we generate a function like this:
@@ -26,10 +26,10 @@ static int branch(long a, long b, long c)
 	struct sljit_jump *out;
 
 	/* Create a SLJIT compiler */
-	struct sljit_compiler *C = sljit_create_compiler();
+	struct sljit_compiler *C = sljit_create_compiler(NULL);
 
 	/* 3 arg, 1 temp reg, 3 save reg */
-	sljit_emit_enter(C, 0,  3,  1, 3, 0, 0, 0);
+	sljit_emit_enter(C, 0, SLJIT_ARG1(SW)|SLJIT_ARG2(SW)|SLJIT_ARG3(SW),  3, 3, 0, 0, 0);
 
 	/* R0 = a & 1, S0 is argument a */
 	sljit_emit_op2(C, SLJIT_AND, SLJIT_R0, 0, SLJIT_S0, 0, SLJIT_IMM, 1);

doc/tutorial/first_program.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef long SLJIT_CALL (*func3_t)(long a, long b, long c);
+typedef SLJIT_FUNC long (*func3_t)(long a, long b, long c);
 
 static int add3(long a, long b, long c)
 {
@@ -12,10 +12,10 @@ static int add3(long a, long b, long c)
 	func3_t func;
 
 	/* Create a SLJIT compiler */
-	struct sljit_compiler *C = sljit_create_compiler();
+	struct sljit_compiler *C = sljit_create_compiler(NULL);
 
 	/* Start a context(function entry), have 3 arguments, discuss later */
-	sljit_emit_enter(C, 0,  3,  1, 3, 0, 0, 0);
+	sljit_emit_enter(C, 0,  SLJIT_ARG1(SW)|SLJIT_ARG2(SW)|SLJIT_ARG3(SW),  3, 3, 0, 0, 0);
 
 	/* The first arguments of function is register SLJIT_S0, 2nd, SLJIT_S1, etc.  */
 	/* R0 = first */

doc/tutorial/func_call.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef long SLJIT_CALL (*func3_t)(long a, long b, long c);
+typedef SLJIT_FUNC long (*func3_t)(long a, long b, long c);
 
-static long SLJIT_CALL print_num(long a)
+static SLJIT_FUNC long print_num(long a)
 {
 	printf("a = %ld\n", a);
 	return a + 1;
@@ -32,9 +32,9 @@ static int func_call(long a, long b, long c)
 	struct sljit_jump *print_c;
 
 	/* Create a SLJIT compiler */
-	struct sljit_compiler *C = sljit_create_compiler();
+	struct sljit_compiler *C = sljit_create_compiler(NULL);
 
-	sljit_emit_enter(C, 0,  3,  1, 3, 0, 0, 0);
+	sljit_emit_enter(C, 0, SLJIT_ARG1(SW)|SLJIT_ARG2(SW)|SLJIT_ARG3(SW), 3, 3, 0, 0, 0);
 
 	/*  a & 1 --> R0 */
 	sljit_emit_op2(C, SLJIT_AND, SLJIT_R0, 0, SLJIT_S0, 0, SLJIT_IMM, 1);
@@ -43,7 +43,7 @@ static int func_call(long a, long b, long c)
 
 	/* R0 = S1; print_num(R0) */
 	sljit_emit_op1(C, SLJIT_MOV, SLJIT_R0, 0, SLJIT_S1, 0);
-	sljit_emit_ijump(C, SLJIT_CALL1, SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));
+	sljit_emit_icall(C, SLJIT_CALL, SLJIT_RET(SW) | SLJIT_ARG1(SW), SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));
 
 	/* jump out */
 	out = sljit_emit_jump(C, SLJIT_JUMP);
@@ -52,7 +52,7 @@ static int func_call(long a, long b, long c)
 
 	/* R0 = c; print_num(R0); */
 	sljit_emit_op1(C, SLJIT_MOV, SLJIT_R0, 0, SLJIT_S2, 0);
-	sljit_emit_ijump(C, SLJIT_CALL1, SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));
+	sljit_emit_icall(C, SLJIT_CALL, SLJIT_RET(SW) | SLJIT_ARG1(SW), SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));
 
 	/* out: */
 	sljit_set_label(out, sljit_emit_label(C));

doc/tutorial/loop.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef long (*func2_t)(long a, long b);
+typedef SLJIT_FUNC long (*func2_t)(long a, long b);
 
 /*
   This example, we generate a function like this:
@@ -29,10 +29,10 @@ static int loop(long a, long b)
 	struct sljit_jump *out;
 
 	/* Create a SLJIT compiler */
-	struct sljit_compiler *C = sljit_create_compiler();
+	struct sljit_compiler *C = sljit_create_compiler(NULL);
 
 	/* 2 arg, 2 temp reg, 2 saved reg */
-	sljit_emit_enter(C, 0, 2, 2, 2, 0, 0, 0);
+	sljit_emit_enter(C, 0, SLJIT_ARG1(SW)|SLJIT_ARG2(SW), 2, 2, 0, 0, 0);
 
 	/* R0 = 0 */
 	sljit_emit_op2(C, SLJIT_XOR, SLJIT_R1, 0, SLJIT_R1, 0, SLJIT_R1, 0);

doc/tutorial/struct_access.c
@@ -8,12 +8,11 @@ struct point_st {
 	int y;
 	short z;
 	char d;
-	char e;
 };
 
-typedef long (*point_func_t)(struct point_st *point);;
+typedef SLJIT_FUNC long (*point_func_t)(struct point_st *point);;
 
-static long SLJIT_CALL print_num(long a)
+static SLJIT_FUNC long print_num(long a)
 {
 	printf("a = %ld\n", a);
 	return a + 1;
@@ -39,26 +38,26 @@ static int struct_access()
 	point_func_t func;
 
 	struct point_st point = {
-		-5, -20, 5, ' ', 'a'
+		-5, -20, 5, 'a'
 	};
 
 	/* Create a SLJIT compiler */
-	struct sljit_compiler *C = sljit_create_compiler();
+	struct sljit_compiler *C = sljit_create_compiler(NULL);
 
-	sljit_emit_enter(C, 0,  1,  1, 1, 0, 0, 0);
+	sljit_emit_enter(C, 0, SLJIT_ARG1(SW), 1, 1, 0, 0, 0);
 	/*                  opt arg R  S  FR FS local_size */
 
 	sljit_emit_op1(C, SLJIT_MOV, SLJIT_R0, 0, SLJIT_MEM1(SLJIT_S0), SLJIT_OFFSETOF(struct point_st, x));	// S0->x --> R0
-	sljit_emit_ijump(C, SLJIT_CALL1, SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));								// print_num(R0);
+	sljit_emit_icall(C, SLJIT_CALL, SLJIT_RET(SW)|SLJIT_ARG1(SW), SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));								// print_num(R0);
 
-	sljit_emit_op1(C, SLJIT_MOV_SI, SLJIT_R0, 0, SLJIT_MEM1(SLJIT_S0), SLJIT_OFFSETOF(struct point_st, y));	// S0->y --> R0
-	sljit_emit_ijump(C, SLJIT_CALL1, SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));								// print_num(R0);
+	sljit_emit_op1(C, SLJIT_MOV_S32, SLJIT_R0, 0, SLJIT_MEM1(SLJIT_S0), SLJIT_OFFSETOF(struct point_st, y));	// S0->y --> R0
+	sljit_emit_icall(C, SLJIT_CALL, SLJIT_RET(SW)|SLJIT_ARG1(SW), SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));								// print_num(R0);
 
-	sljit_emit_op1(C, SLJIT_MOV_SH, SLJIT_R0, 0, SLJIT_MEM1(SLJIT_S0), SLJIT_OFFSETOF(struct point_st, z));	// S0->z --> R0
-	sljit_emit_ijump(C, SLJIT_CALL1, SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));								// print_num(R0);
+	sljit_emit_op1(C, SLJIT_MOV_S16, SLJIT_R0, 0, SLJIT_MEM1(SLJIT_S0), SLJIT_OFFSETOF(struct point_st, z));	// S0->z --> R0
+	sljit_emit_icall(C, SLJIT_CALL, SLJIT_RET(SW)|SLJIT_ARG1(SW), SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));								// print_num(R0);
 
-	sljit_emit_op1(C, SLJIT_MOV_SB, SLJIT_R0, 0, SLJIT_MEM1(SLJIT_S0), SLJIT_OFFSETOF(struct point_st, d));	// S0->z --> R0
-	sljit_emit_ijump(C, SLJIT_CALL1, SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));								// print_num(R0);
+	sljit_emit_op1(C, SLJIT_MOV_S8, SLJIT_R0, 0, SLJIT_MEM1(SLJIT_S0), SLJIT_OFFSETOF(struct point_st, d));	// S0->z --> R0
+	sljit_emit_icall(C, SLJIT_CALL, SLJIT_RET(SW)|SLJIT_ARG1(SW), SLJIT_IMM, SLJIT_FUNC_OFFSET(print_num));								// print_num(R0);
 
 	sljit_emit_return(C, SLJIT_MOV, SLJIT_MEM1(SLJIT_S0), SLJIT_OFFSETOF(struct point_st, x));				// return S0->x
 

commit 6ddb51e7b517c3d96679c72a73c3d28554006909

build: reintroduce bin directory dependency
after the bin directory was removed as a dependency to cleanup (and fix)
the dependency tracking, the following scenario is possibly breaking
builds:

  $ git clean -fxd
  Removing bin/
  $ make
  cc  -DSLJIT_CONFIG_AUTO=1 -Isljit_src -O2 -Wall -c -o bin/sljitMain.o test_src/sljitMain.c
  error: unable to open output file 'bin/sljitMain.o': 'No such file or directory'
  1 error generated.
  make: *** [bin/sljitMain.o] Error 1

luckily I already fixed this problem in my "makefile" branch as
carenas/sljit@5618ec91b85df8427f38e36f850e1f828501aed4 so
implement it now by adding a sentinel to the bin directory and using
that instead as a dependency.

while at it, fix some other minor issues from that branch that are
hopefully something that we could agree on and might had even been
discussed recently.

CC: @nymacro
Fixes: fcf394a54711946ff938c084d8123e4af9940db2

Makefile
@@ -31,7 +31,8 @@ EXAMPLE_TARGET = $(BINDIR)/func_call $(BINDIR)/first_program $(BINDIR)/branch $(
 
 SLJIT_HEADERS = $(SRCDIR)/sljitLir.h $(SRCDIR)/sljitConfig.h $(SRCDIR)/sljitConfigInternal.h
 
-SLJIT_LIR_FILES = $(SRCDIR)/sljitLir.c $(SRCDIR)/sljitExecAllocator.c $(SRCDIR)/sljitUtils.c \
+SLJIT_LIR_FILES = $(SRCDIR)/sljitLir.c $(SRCDIR)/sljitUtils.c \
+	$(SRCDIR)/sljitExecAllocator.c $(SRCDIR)/sljitProtExecAllocator.c \
 	$(SRCDIR)/sljitNativeARM_32.c $(SRCDIR)/sljitNativeARM_T2_32.c $(SRCDIR)/sljitNativeARM_64.c \
 	$(SRCDIR)/sljitNativeMIPS_common.c $(SRCDIR)/sljitNativeMIPS_32.c $(SRCDIR)/sljitNativeMIPS_64.c \
 	$(SRCDIR)/sljitNativePPC_common.c $(SRCDIR)/sljitNativePPC_32.c $(SRCDIR)/sljitNativePPC_64.c \
@@ -39,60 +40,60 @@ SLJIT_LIR_FILES = $(SRCDIR)/sljitLir.c $(SRCDIR)/sljitExecAllocator.c $(SRCDIR)/
 	$(SRCDIR)/sljitNativeTILEGX_64.c \
 	$(SRCDIR)/sljitNativeX86_common.c $(SRCDIR)/sljitNativeX86_32.c $(SRCDIR)/sljitNativeX86_64.c
 
-.PHONY: all examples
+.PHONY: all clean examples
 
 all: $(TARGET)
 
-$(BINDIR) :
-	mkdir $(BINDIR)
+clean:
+	-$(RM) $(BINDIR)/*.o $(BINDIR)/sljit_test $(BINDIR)/regex_test $(EXAMPLE_TARGET)
 
-$(BINDIR)/sljitLir.o : $(SLJIT_LIR_FILES) $(SLJIT_HEADERS)
+$(BINDIR)/.keep :
+	mkdir -p $(BINDIR)
+	@touch $@
+
+$(BINDIR)/sljitLir.o : $(BINDIR)/.keep $(SLJIT_LIR_FILES) $(SLJIT_HEADERS)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c -o $@ $(SRCDIR)/sljitLir.c
 
-$(BINDIR)/sljitMain.o : $(TESTDIR)/sljitMain.c $(SLJIT_HEADERS)
+$(BINDIR)/sljitMain.o : $(TESTDIR)/sljitMain.c $(BINDIR)/.keep $(SLJIT_HEADERS)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c -o $@ $(TESTDIR)/sljitMain.c
 
-$(BINDIR)/sljitTest.o : $(TESTDIR)/sljitTest.c $(SLJIT_HEADERS)
+$(BINDIR)/sljitTest.o : $(TESTDIR)/sljitTest.c $(BINDIR)/.keep $(SLJIT_HEADERS)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c -o $@ $(TESTDIR)/sljitTest.c
 
-$(BINDIR)/regexMain.o : $(REGEXDIR)/regexMain.c $(SLJIT_HEADERS)
+$(BINDIR)/regexMain.o : $(REGEXDIR)/regexMain.c $(BINDIR)/.keep $(SLJIT_HEADERS)
 	$(CC) $(CPPFLAGS) $(REGEX_CFLAGS) -c -o $@ $(REGEXDIR)/regexMain.c
 
-$(BINDIR)/regexJIT.o : $(REGEXDIR)/regexJIT.c $(SLJIT_HEADERS) $(REGEXDIR)/regexJIT.h
+$(BINDIR)/regexJIT.o : $(REGEXDIR)/regexJIT.c $(BINDIR)/.keep $(SLJIT_HEADERS) $(REGEXDIR)/regexJIT.h
 	$(CC) $(CPPFLAGS) $(REGEX_CFLAGS) -c -o $@ $(REGEXDIR)/regexJIT.c
 
-clean:
-	-$(RM) -f $(BINDIR)/*.o $(BINDIR)/sljit_test $(BINDIR)/regex_test $(EXAMPLE_TARGET)
-
-$(BINDIR)/sljit_test: $(BINDIR)/sljitMain.o $(BINDIR)/sljitTest.o $(BINDIR)/sljitLir.o
+$(BINDIR)/sljit_test: $(BINDIR)/.keep $(BINDIR)/sljitMain.o $(BINDIR)/sljitTest.o $(BINDIR)/sljitLir.o
 	$(CC) $(CFLAGS) $(LDFLAGS) $(BINDIR)/sljitMain.o $(BINDIR)/sljitTest.o $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
 
-$(BINDIR)/regex_test: $(BINDIR)/regexMain.o $(BINDIR)/regexJIT.o $(BINDIR)/sljitLir.o
+$(BINDIR)/regex_test: $(BINDIR)/.keep $(BINDIR)/regexMain.o $(BINDIR)/regexJIT.o $(BINDIR)/sljitLir.o
 	$(CC) $(CFLAGS) $(LDFLAGS) $(BINDIR)/regexMain.o $(BINDIR)/regexJIT.o $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
-
 
 examples: $(EXAMPLE_TARGET)
 
-$(BINDIR)/first_program: $(EXAMPLEDIR)/first_program.c $(BINDIR)/sljitLir.o
+$(BINDIR)/first_program: $(EXAMPLEDIR)/first_program.c $(BINDIR)/.keep $(BINDIR)/sljitLir.o
 	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/first_program.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
 
-$(BINDIR)/branch: $(EXAMPLEDIR)/branch.c $(BINDIR)/sljitLir.o
+$(BINDIR)/branch: $(EXAMPLEDIR)/branch.c $(BINDIR)/.keep $(BINDIR)/sljitLir.o
 	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/branch.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
 
-$(BINDIR)/loop: $(EXAMPLEDIR)/loop.c $(BINDIR)/sljitLir.o
+$(BINDIR)/loop: $(EXAMPLEDIR)/loop.c $(BINDIR)/.keep $(BINDIR)/sljitLir.o
 	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/loop.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
 
-$(BINDIR)/array_access: $(EXAMPLEDIR)/array_access.c $(BINDIR)/sljitLir.o
+$(BINDIR)/array_access: $(EXAMPLEDIR)/array_access.c $(BINDIR)/.keep $(BINDIR)/sljitLir.o
 	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/array_access.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
 
-$(BINDIR)/func_call: $(EXAMPLEDIR)/func_call.c $(BINDIR)/sljitLir.o
+$(BINDIR)/func_call: $(EXAMPLEDIR)/func_call.c $(BINDIR)/.keep $(BINDIR)/sljitLir.o
 	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/func_call.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
 
-$(BINDIR)/struct_access: $(EXAMPLEDIR)/struct_access.c $(BINDIR)/sljitLir.o
+$(BINDIR)/struct_access: $(EXAMPLEDIR)/struct_access.c $(BINDIR)/.keep $(BINDIR)/sljitLir.o
 	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/struct_access.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
 
-$(BINDIR)/temp_var: $(EXAMPLEDIR)/temp_var.c $(BINDIR)/sljitLir.o
+$(BINDIR)/temp_var: $(EXAMPLEDIR)/temp_var.c $(BINDIR)/.keep $(BINDIR)/sljitLir.o
 	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/temp_var.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread
 
-$(BINDIR)/brainfuck: $(EXAMPLEDIR)/brainfuck.c $(BINDIR)/sljitLir.o
+$(BINDIR)/brainfuck: $(EXAMPLEDIR)/brainfuck.c $(BINDIR)/.keep $(BINDIR)/sljitLir.o
 	$(CC) $(CPPFLAGS) $(LDFLAGS) $(EXAMPLEDIR)/brainfuck.c $(BINDIR)/sljitLir.o -o $@ -lm -lpthread