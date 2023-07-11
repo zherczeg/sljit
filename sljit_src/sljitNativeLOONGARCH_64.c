cd4f7ab20b1ab67c71320c831d4b7c40feb8da4e
Carlo Marcelo Arenas Belón <carenas@gmail.com>
2023-07-11 17:39:14 +0800

    loongarch: implement copysign
    While at it, update cpu identification to avoid duplicated "64".

--- sljit_src/sljitNativeLOONGARCH_64.c
+++ sljit_src/sljitNativeLOONGARCH_64.c
@@ -27,7 +27,7 @@
 SLJIT_API_FUNC_ATTRIBUTE const char* sljit_get_platform_name(void)
 {
 #if (defined SLJIT_CONFIG_LOONGARCH_64 && SLJIT_CONFIG_LOONGARCH_64)
-	return "LOONGARCH-64" SLJIT_CPUINFO;
+	return "LOONGARCH" SLJIT_CPUINFO;
 #endif /* SLJIT_CONFIG_LOONGARCH_64 */
 }
 
@@ -262,6 +262,8 @@ lower parts in the instruction word, denoted by the “L” and “H” suffixes
 #define FDIV_D  OPC_3R(0x20e)
 #define FCMP_COND_S  OPC_4R(0xc1)
 #define FCMP_COND_D  OPC_4R(0xc2)
+#define FCOPYSIGN_S  OPC_3R(0x225)
+#define FCOPYSIGN_D  OPC_3R(0x226)
 #define FSEL  OPC_4R(0xd0)
 #define FABS_S  OPC_2R(0x4501)
 #define FABS_D  OPC_2R(0x4502)
@@ -2351,18 +2353,17 @@ SLJIT_API_FUNC_ATTRIBUTE sljit_s32 sljit_emit_fop2(struct sljit_compiler *compil
 	case SLJIT_ADD_F64:
 		FAIL_IF(push_inst(compiler, FINST(FADD, op) | FRD(dst_r) | FRJ(src1) | FRK(src2)));
 		break;
-
 	case SLJIT_SUB_F64:
 		FAIL_IF(push_inst(compiler, FINST(FSUB, op) | FRD(dst_r) | FRJ(src1) | FRK(src2)));
 		break;
-
 	case SLJIT_MUL_F64:
 		FAIL_IF(push_inst(compiler, FINST(FMUL, op) | FRD(dst_r) | FRJ(src1) | FRK(src2)));
 		break;
-
 	case SLJIT_DIV_F64:
 		FAIL_IF(push_inst(compiler, FINST(FDIV, op) | FRD(dst_r) | FRJ(src1) | FRK(src2)));
 		break;
+	case SLJIT_COPYSIGN_F64:
+		return push_inst(compiler, FINST(FCOPYSIGN, op) | FRD(dst_r) | FRJ(src1) | FRK(src2));
 	}
 
 	if (dst_r == TMP_FREG2)
